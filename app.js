1502d84507b509f5d31296d8ff138e8b27780abc
KillianRvt <killian.riveault@ynov.com>
2018-11-13 15:45:01 +0100

    First Commit with chat, list of users but no private messages

---
+++ app.js
@@ -0,0 +1,44 @@
+const app = require('express')();
+const http  = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = 4096
+
+io.on('connection', socket => {
+    console.log('user connected : ', socket.id);
+    let allSocketId = Object.keys(io.sockets.clients().connected);
+    console.log(allSocketId);
+    socket.on('loaded', (data) => {
+        console.log('data from client : ', data)
+    })
+
+    socket.on('user', (data)=>{
+        socket.pseudo=data;
+        generateListUser();
+    })
+    socket.on('message', (data) => {
+        socket.broadcast.emit('message', data)
+        //io.emit('message', data)
+    })
+    io.emit('userList', allSocketId)
+    function generateListUser(){
+        var userList = [];
+        console.log(userList);
+        var clientList = io.sockets.clients().connected
+        Object.entries(clientList).forEach(element => {
+            let pseudo = element[1].pseudo;
+            let id = element[1].id;
+            userList.push({'id': id, 'pseudo': pseudo})
+        });
+        io.emit('generateList', userList)
+    }
+});
+
+app.get('/', (req, res)=> {
+    //res.send(userList);
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+http.listen(port, ()=> {
+    console.log('Server is up and running on http://localhost:'+port);
+}); 
